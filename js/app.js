a643a4f0e42c05b7bc41a458024620eb04d4aaed
Zuhair.N <Zuhairnawasreh@gmail.com>
2022-05-27 22:36:00 +0300

    add navbar with js part

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 'use strict';
+const nav = document.querySelector('.nav');
+const navLogo = document.querySelector('.nav-logo');
+
+navLogo.addEventListener('click', (e) => {
+  if (nav.getAttribute('aria-expanded') === 'true') {
+    nav.setAttribute('aria-expanded', 'false');
+  } else {
+    nav.setAttribute('aria-expanded', 'true');
+  }
+});
