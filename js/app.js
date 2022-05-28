2ab6faa449d0e1d7820259cd6138f1e421f37745
Zuhair.N <Zuhairnawasreh@gmail.com>
2022-05-28 23:56:54 +0300

    add shorten link API

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 'use strict';
 const nav = document.querySelector('.nav');
 const navLogo = document.querySelector('.nav-logo');
+const form = document.querySelector('form');
+const section2 = document.querySelector('.section-2');
 
 navLogo.addEventListener('click', (e) => {
   if (nav.getAttribute('aria-expanded') === 'true') {
@@ -9,3 +11,36 @@ navLogo.addEventListener('click', (e) => {
     nav.setAttribute('aria-expanded', 'true');
   }
 });
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const url = e.target.elements.url.value;
+  console.log(url);
+  const urlShortener = async () => {
+    try {
+      const res = await axios.get(
+        `https://api.shrtco.de/v2/shorten?url=${url}`
+      );
+      const data = res.data.result;
+      console.log(data.short_link, data.original_link);
+
+      const linkCard = document.createElement('div');
+      linkCard.setAttribute('class', 'linkCard');
+      section2.prepend(linkCard);
+      const link = document.createElement('p');
+      link.textContent = data.original_link;
+      linkCard.append(link);
+      const shLink = document.createElement('p');
+      shLink.textContent = data.short_link;
+      linkCard.append(shLink);
+      const cpbtn = document.createElement('button');
+      cpbtn.textContent = 'Copy';
+      linkCard.append(cpbtn);
+    } catch (e) {
+      alert('please try again');
+      console.log(e);
+    }
+  };
+  urlShortener();
+});
+// need save data in array with local storage
